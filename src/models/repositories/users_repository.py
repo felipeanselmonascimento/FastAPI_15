bad098f2ef8262a9a16aaa8fcb0381cb24d79eff
Felipe Anselmo <felipeanselmodonascimento@gmail.com>
2026-04-15 23:19:25 +0100

    added user repositorie

---
+++ src/models/repositories/users_repository.py
@@ -0,0 +1,15 @@
+from sqlalchemy import insert
+from src.models.entities.users import Users
+from src.models.settings.database_connection_handler import DBConnectionHandler
+
+
+class UsersRepository:
+    async def insert_users(self, user_infos: dict) -> None:
+        async with DBConnectionHandler() as db:
+            query = insert(Users).values(**user_infos)
+            await db.session.execute(query)
+            await db.session.commit()
+
+            # O ** "desempacota" o dicionário — é igual o spread do JS:
+
+            # dict É igual ao objeto do JS!
